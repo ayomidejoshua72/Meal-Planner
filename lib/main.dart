07cd104193113a63344e07d3c53968eb3c2a4db3
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-08 23:12:50 +0100

    add font to theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:todo_list/pages/home_page.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp( MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomePage(),
+    theme: ThemeData(fontFamily: "Poppins"),
+    home: const HomePage(),
   ));
 }
