3f300ed7d9c3950b2dbd860efc66c3ab45191000
ayomidejoshua72 <ayomidejoshua72@gmail.com>
2023-12-08 23:44:37 +0100

    add background color to scaffold

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -10,6 +10,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          
+        ],
+      ),
     );
   }
 
